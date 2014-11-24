73052ff0f0a5f9b8030c1806dc66d3cb9be032ce
Rex St John <rstjohn@Rex-St-John--Intel.local>
2014-11-23 18:21:20 -0800

    bug fixes

--- EdisonGatt/edison-device-information-service.js
+++ EdisonGatt/edison-device-information-service.js
@@ -1,40 +1,21 @@
 /**
-* Expose the device information of the Edison device
-*
-* Reference: https://developer.bluetooth.org/gatt/services/Pages/ServiceViewer.aspx?u=org.bluetooth.service.device_information.xml
-*/
-
-var util = require('util'),
-  os = require('os'),
-  exec = require('child_process').exec,
-  bleno = require('bleno'),
-  Descriptor = bleno.Descriptor,
-  Characteristic = bleno.Characteristic;
-
-/**
 * Reference:
 * https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.manufacturer_name_string.xml
 */
-var EdisonDeviceStatusCharacteristic = function() {
-  EdisonDeviceStatusCharacteristic.super_.call(this, {
-      uuid: '2A29',
-      properties: ['read'],
-      descriptors: [
-        new Descriptor({
-        uuid: '2901',
-        value: 'Device Status'
-      })]
+var util = require('util'),
+  bleno = require('bleno'),
+  BlenoPrimaryService = bleno.PrimaryService,
+  EdisonDeviceManufacturerCharacteristic = require('./edison-device-manufacturer-characteristic');
+
+// Everything we want to be able to read / write about Edison using our paired mobile app.
+function EdisonDeviceInformationService() {
+  EdisonDeviceInformationService.super_.call(this, {
+      uuid: '180A',
+      characteristics: [
+          new EdisonDeviceManufacturerCharacteristic()
+      ]
   });
-};
+}
 
-util.inherits(EdisonDeviceStatusCharacteristic, Characteristic);
-
-EdisonDeviceStatusCharacteristic.prototype.onReadRequest = function(offset, callback) {
- if (offset) {
-    callback(this.RESULT_ATTR_NOT_LONG, null);
-  } else {
-    callback(this.RESULT_SUCCESS, new Buffer('online'));
-  }
-};
-
-module.exports = EdisonDeviceStatusCharacteristic;
+util.inherits(EdisonDeviceInformationService, BlenoPrimaryService);
+module.exports = EdisonDeviceInformationService;
